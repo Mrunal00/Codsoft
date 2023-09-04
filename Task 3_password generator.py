3e4f35dcc83317ead25484a63bc8422b6a0deb50
Mrunal00 <143897905+Mrunal00@users.noreply.github.com>
2023-09-04 17:04:38 +0530

    Update Task 3_password generator.py

--- Task 3_password generator.py
+++ Task 3_password generator.py
@@ -1,108 +1,103 @@
 from tkinter import *
-root = Tk()
-root.title("To-Do-List")
-root.resizable(False,False)
-root.geometry("750x750+0+0")
+import random
+
+# Define constants
+RED = "#e7305b"
+PURPLE = "#9000a0"
+YELLOW = "#f7f5dd"
+BLACK = "#000000"
+FONT_NAME = "Courier"
 
 
+# Logic for generating password
+def generate_password():
+    # Clearing the password field
+    password_field.delete(1, 99999999)
 
-def q1_correct():
-    l2=Label(f1, text="Correct", font="Arial 18 bold", bg="green",fg="black")
-    l2.place(x=600,y=150)
-    ll2=Label(f1, text="Score: 1", font="Arial 18 bold",fg="black")
-    ll2.place(x=600,y=100)
+    # Collection of letters, numbers and symbols that are used to generate password
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-def q1_incorrect():
-    l3=Label(f1, text="Wrong", font="Arial 18 bold", bg="red",fg="black")
-    l3.place(x=600,y=150)
-    ll3=Label(f1, text="Score: 0", font="Arial 18 bold",fg="black")
-    ll3.place(x=600,y=100)
+    # Getting no. of letters, symbols and numbers from spinboxes
+    nr_letters = int(letter_input.get())
+    nr_symbols = int(symbols_input.get())
+    nr_numbers = int(number_input.get())
 
-def q2_correct():
-    l4=Label(f1, text="Correct", font="Arial 18 bold", bg="green",fg="black")
-    l4.place(x=600,y=300)
-    ll4=Label(f1, text="Score: 1", font="Arial 18 bold",fg="black")
-    ll4.place(x=600,y=250)
+    # Initialising an empty password array
+    password = []
 
-def q2_incorrect():
-    l5=Label(f1, text="Wrong", font="Arial 18 bold", bg="red",fg="black")
-    l5.place(x=600,y=300)
-    ll5=Label(f1, text="Score: 0", font="Arial 18 bold",fg="black")
-    ll5.place(x=600,y=250)
+    # Adding letters, symbols and numbers to the password array
+    for letter in range(nr_letters):
+        password += random.choice(letters)
 
-def q3_correct():
-    l6=Label(f1, text="Correct", font="Arial 18 bold", bg="green",fg="black")
-    l6.place(x=600,y=450)
-    ll6=Label(f1, text="Score: 1", font="Arial 18 bold",fg="black")
-    ll6.place(x=600,y=400)
+    for symbol in range(nr_symbols):
+        password += random.choice(symbols)
 
-def q3_incorrect():
-    l7=Label(f1, text="Wrong", font="Arial 18 bold", bg="red",fg="black")
-    l7.place(x=600,y=450)
-    ll7=Label(f1, text="Score: 0", font="Arial 18 bold",fg="black")
-    ll7.place(x=600,y=400)
+    for letter in range(nr_numbers):
+        password += random.choice(numbers)
 
-def q4_correct():
-    l8=Label(f1, text="Correct", font="Arial 18 bold", bg="green",fg="black")
-    l8.place(x=600,y=600)
-    ll8=Label(f1, text="Score: 1", font="Arial 18 bold",fg="black")
-    ll8.place(x=600,y=550)
+    # Randomly shuffling the password array to make the password scrambled
+    random.shuffle(password)
 
-def q4_incorrect():
-    l9=Label(f1, text="Wrong", font="Arial 18 bold", bg="red",fg="black")
-    l9.place(x=600,y=600)
-    ll9=Label(f1, text="Score: 0", font="Arial 18 bold",fg="black")
-    ll9.place(x=600,y=550)
+    # Initialise an empty password string
+    new_pass = ""
+
+    # Add all characters from password array to the password string
+    for char in password:
+        new_pass += char
+
+    # Set the value of Password Field to the Generated Password
+    password_field.insert(index=0, string=new_pass)
 
 
-#Frame
-f1 =Frame(root,width = 1000,height = 1000,bg="grey")
-f1.place(x=0,y=0)
+# GUI using tkinter
+window = Tk()
 
+# Label for the title
+title_label = Label(text="Welcome To Password Generator!", bg=YELLOW, fg=PURPLE, font=(FONT_NAME, 45))
+title_label.pack()
 
-#heading
-heading=Label(root,text="Quiz-Game",font="boulder 24 bold",fg="black",bg="lightblue").pack()
+# Label for Number of Letters
+letter_label = Label(text="1. How many letters would you like in your password?", bg=YELLOW, fg=BLACK, font=(FONT_NAME,
+                                                                                                             20))
+letter_label.place(x=10, y=100)
 
+# Spinbox for Number of Letters
+letter_input = Spinbox(from_=0, to=20, width=10, font=(FONT_NAME, 20))
+letter_input.place(x=675, y=100)
 
-#Q1
-l1=Label(f1, text="Q1. What is the Capital of Uganda ?", font="Arial 14 bold",fg="Black",bg="grey")
-l1.place(x=0,y=100)
-b1=Button(f1, text="Kampala",font="Arial 12 bold",bg="grey",command=q1_correct)
-b1.place(x=0,y=150)
-b2=Button(f1, text="Singapur",font="Arial 12 bold",bg="grey",command=q1_incorrect)
-b2.place(x=110,y=150)
-b3=Button(f1, text="Hongkong",font="Arial 12 bold",bg="grey",command=q1_incorrect)
-b3.place(x=222,y=150)
+# Label for Number of Symbols
+symbols_label = Label(text="2. How many symbols would you like?", bg=YELLOW, fg=BLACK, font=(FONT_NAME, 20))
+symbols_label.place(x=10, y=150)
 
-#Q2
-l1=Label(f1, text="Q2. How many Vowels are there in English Alphabet ?", font="Arial 14 bold",fg="Black",bg="grey")
-l1.place(x=0,y=250)
-b1=Button(f1, text="5",font="Arial 12 bold",bg="grey",command=q2_correct)
-b1.place(x=0,y=300)
-b2=Button(f1, text="6",font="Arial 12 bold",bg="grey",command=q2_incorrect)
-b2.place(x=110,y=300)
-b3=Button(f1, text="4",font="Arial 12 bold",bg="grey",command=q2_incorrect)
-b3.place(x=222,y=300)
+# Spinbox for Number of Symbols
+symbols_input = Spinbox(from_=0, to=20, width=10, font=(FONT_NAME, 20))
+symbols_input.place(x=675, y=150)
 
-#Q3
-l1=Label(f1, text="Q3. What is the Output of 4x3 ?", font="Arial 14 bold",fg="Black",bg="grey")
-l1.place(x=0,y=400)
-b1=Button(f1, text="13",font="Arial 12 bold",bg="grey",command=q3_incorrect)
-b1.place(x=0,y=450)
-b2=Button(f1, text="12",font="Arial 12 bold",bg="grey",command=q3_correct)
-b2.place(x=110,y=450)
-b3=Button(f1, text="11",font="Arial 12 bold",bg="grey",command=q3_incorrect)
-b3.place(x=222,y=450)
+# Label for Number of Numbers
+number_label = Label(text="3. How many numbers would you like?", bg=YELLOW, fg=BLACK, font=(FONT_NAME, 20))
+number_label.place(x=10, y=200)
 
-#Q4
-l1=Label(f1, text="Q4. What is the total number of Alphabet in English ?", font="Arial 14 bold",fg="Black",bg="grey")
-l1.place(x=0,y=550)
-b1=Button(f1, text="30",font="Arial 12 bold",bg="grey",command=q4_incorrect)
-b1.place(x=0,y=600)
-b2=Button(f1, text="27",font="Arial 12 bold",bg="grey",command=q4_incorrect)
-b2.place(x=110,y=600)
-b3=Button(f1, text="26",font="Arial 12 bold",bg="grey",command=q4_correct)
-b3.place(x=222,y=600)
+# Spinbox for Number of Numbers
+number_input = Spinbox(from_=0, to=20, width=10, font=(FONT_NAME, 20))
+number_input.place(x=675, y=200)
 
+# Button for Generating Password
+generate_button = Button(text="Generate", bg=PURPLE, font=(FONT_NAME, 20), command=generate_password)
+generate_button.pack(pady=200)
 
-root.mainloop()
+# Field for the Generated Password
+password_field = Entry(width=42, font=(FONT_NAME, 30), fg=RED)
+password_field.place(x=10, y=350)
+
+# Screen configurations
+window.title("Password Generator App")
+window.config(padx=100, pady=50, bg=YELLOW)
+window.geometry("+270+100")
+window.minsize(width=270, height=300)
+window.resizable(False, False)
+
+window.mainloop()
